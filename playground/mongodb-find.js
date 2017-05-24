0ad131b3bac81ae59d3c0117e5093fc63ccacf44
Travis Alcantara <travis.alcantara@gmail.com>
2017-05-24 12:07:33 -0700

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,34 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').find({
+  //     _id: new ObjectID('5925c3f631338200e58dcfec')
+  //   }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find({name: 'Travis Alcantara'}).toArray().then((docs) => {
+    console.log('Users named Travis Alcantara');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch users');
+  });
+
+  //db.close();
+});
